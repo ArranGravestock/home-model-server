eba03d24aefc2d7a569c19b59d2512822ff9b971
arranGravestock <arran@gravestock.net>
2018-02-23 17:18:58 +0000

    add led blink add hc-sr04 sensor

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
+//run setups
 var wpi = require('wiring-pi');
-
+//var usonic = require('r-pi-usonic');
 wpi.setup('wpi');
 
 //pins
-var pin = 7;
+var ledpin = 7;
+var trigpin = 11;
+var echopin = 12;
 
-wpi.pinMode(pin, wpi.OUTPUT);
+//set mode
+wpi.pinMode(ledpin, wpi.OUTPUT);
+wpi.pinMode(trigpin, wpi.OUTPUT);
+wpi.pinMode(echopin, wpi.INPUT);
+
+function readUsonicSensor() {
+	wpi.digitalWrite(trigpin, 0);
+	wpi.delay(50);
+	wpi.digitalWrite(trigpin, 1);
+	wpi.delay(100);
+	wpi.digitalWrite(trigpin, 0);
+	
+	var pulse_start = wpi.pulseIn(echopin, 0);
+	var pulse_end = wpi.pulseIn(echopin, 1);
+	var pulse_duration = pulse_end - pulse_start;
+	var distance = pulse_duration * 17150;
+	return distance;
+}
 
 var value = 0;
 setInterval(function() {
-	wpi.digitalWrite(pin, value);
-value = +!value;
-readPin(pin);
+	wpi.digitalWrite(ledpin, value);
+	value = +!value;
+
+
+	var usonicDistance = readUsonicSensor();
+	console.log(usonicDistance);
 }, 500);
 
 function readPin(inputPin) {
@@ -20,4 +43,10 @@ function readPin(inputPin) {
 	var json = JSON.stringify(stringBuilder);
 	console.log(json);
 }
+
+function initUsonicSensor() {
+	var sensor = usonic.createSensor(echopin, trigpin, 450);
+	
+	setTimeout(function() { console.log(sensor().toFixed(2)) }, 60);
+};
 	
