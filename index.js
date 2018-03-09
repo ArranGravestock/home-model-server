6236a94279072177d9538be5c54ff60989a3e892
arranGravestock <arran@gravestock.net>
2018-03-09 19:40:53 +0000

    add driver motor and water sensor

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ var echopin = 28;
 var motion = 4;
 var touch = 3;
 
+var spiclk = 14;
+var spimiso = 12;
+var spimosi = 13;
+var spics = 10;
+
+var motor_one = 22;
+var motor_two = 23;
+var motor_three = 24;
+var motor_four = 25;
+
 //voltage init
 var LOW = 0;
 //var HIGH = 1;
@@ -21,6 +31,16 @@ wpi.pinMode(trigpin, wpi.OUTPUT);
 wpi.pinMode(echopin, wpi.INPUT);
 wpi.pinMode(motion, wpi.INPUT);
 wpi.pinMode(touch, wpi.INPUT);
+
+wpi.pinMode(motor_one, wpi.OUTPUT);
+wpi.pinMode(motor_two, wpi.OUTPUT);
+wpi.pinMode(motor_three, wpi.OUTPUT);
+wpi.pinMode(motor_four, wpi.OUTPUT);
+
+wpi.pinMode(spimosi, wpi.OUTPUT);
+wpi.pinMode(spimiso, wpi.INPUT);
+wpi.pinMode(spiclk, wpi.OUTPUT);
+wpi.pinMode(spics, wpi.OUTPUT);
 
 function readTouch() {
 	var touch_state = wpi.digitalRead(touch);
@@ -43,8 +63,6 @@ function readMotion() {
 }
 
 function readUsonicSensor() {
-	//var pulse_start = wpi.micros();
-//	var pulse_start = wpi.pulseIn(echopin, 1);
 	wpi.digitalWrite(trigpin, 0);
 	wpi.delayMicroseconds(5);
 	wpi.digitalWrite(trigpin, 1);
@@ -52,10 +70,6 @@ function readUsonicSensor() {
 	wpi.digitalWrite(trigpin, 0);
 	
 	var pulse_start = wpi.pulseIn(echopin, 1);
-	//var pulse_end = wpi.pulseIn(echopin, 1);
-	//var pulse_duration = pulse_end - pulse_start;
-	//var distance = pulse_duration * 17150;
-//	console.log(pulse_start);
 	var distance_cm = pulse_start / 2 / 29.1;
 	console.log(distance_cm.toFixed(2)+ "cm");
 }
@@ -65,10 +79,10 @@ setInterval(function() {
 	wpi.digitalWrite(ledpin, value);
 	value = +!value;
 
-	readUsonicSensor();	
-	readTouch();
-	test();
-	readMotion();	
+//	readUsonicSensor();	
+//	readTouch();
+//	test();
+//	readMotion();	
 }, 300);
 
 function readPin(inputPin) {
@@ -108,4 +122,42 @@ sensor.read(11, 2, function(err, temperature, humidity) {
 		console.log(err);
 	}
     });
+}
+
+var stdin = process.openStdin();
+
+stdin.addListener("data", function(d) {
+	startMotor(d);
+});
+
+function setStep(w1, w2, w3, w4) {
+	wpi.digitalWrite(motor_one, w1);
+	wpi.digitalWrite(motor_two, w2);
+	wpi.digitalWrite(motor_three, w3);
+	wpi.digitalWrite(motor_four, w4);
+}
+
+function startMotor(data) {
+	if (data == 0) {
+		for (var i = 0; i > 100; i++) {
+//		console.log(data + " :data");
+
+		setStep(1,0,0,1);		
+		wpi.delayMicroseconds(1);
+		setStep(0,1,0,1);
+		wpi.delayMicroseconds(1);
+		setStep(0,1,1,0);
+		wpi.delayMicroseconds(1);
+		setStep(1,0,1,0);
+		wpi.delayMicroseconds(1);}
+	} else {
+		setStep(1,0,1,0);
+		wpi.delayMicroseconds(1);
+		setStep(0,1,1,0);
+		wpi.delayMicroseconds(1);
+		setStep(0,1,0,1);
+		wpi.delayMicroseconds(1);
+		setStep(1,0,0,1);
+		wpi.delayMicroseconds(1);
+	}
 }
