9e466a56ad21e608dc2e48936a183b0ea1cb5d3d
ArranGravestock <arran@gravestock.net>
2018-04-14 18:26:58 +0100

    -add led seperation

---
+++ components/led.js
@@ -0,0 +1,51 @@
+class LED {
+	constructor(pin) {
+		this.pin = pin;
+		this.id = SENSOR_ID;
+		this.state = this.fetchState();
+		wpi.pinMode(pin, wpi.OUTPUT);
+
+		SENSOR_ID++;
+	}
+
+	getState() {
+		return this.state;
+	}
+
+	setState(state) {
+		this.state = state;
+		wpi.digitalWrite(this.pin, state);
+	}
+
+	fetchState() {
+		fetch(`http://192.168.1.88:3000/device/${DEVICE_ID}/light/${this.id}`, 
+			{
+				method: 'GET', 
+				credentials: 'include',
+				headers: {
+					'content-type':'application/json',
+					'access-control-allow-origin':'*'
+				}
+			}
+		)
+		.then(res => res.json())
+		.then(json => {
+			var state = json[0].ThingState;
+			//not sure why this is caused...
+			if (state) {
+				this.setState(0)
+			} else {
+				this.setState(1)
+			}
+		})
+		.catch(err => {
+			console.log(err);
+		})
+	}
+
+	getID() {
+		return this.id;
+	}
+}
+
+export default LED;
--- index.js
+++ index.js
@@ -10,6 +10,9 @@ wpi.setup('wpi');
 var LOW = 0;
 var HIGH = 1;
 
+//import classes
+import LED from './components/led';
+
 var DEVICE_ID = "1";
 var SENSOR_ID = 1;
 
@@ -55,55 +58,7 @@ class Fan {
 	}
 }
 
-class LED {
-	constructor(pin) {
-		this.pin = pin;
-		this.id = SENSOR_ID;
-		this.state = this.fetchState();
-		wpi.pinMode(pin, wpi.OUTPUT);
 
-		SENSOR_ID++;
-	}
-
-	getState() {
-		return this.state;
-	}
-
-	setState(state) {
-		this.state = state;
-		wpi.digitalWrite(this.pin, state);
-	}
-
-	fetchState() {
-		fetch(`http://192.168.1.88:3000/device/${DEVICE_ID}/light/${this.id}`, 
-			{
-				method: 'GET', 
-				credentials: 'include',
-				headers: {
-					'content-type':'application/json',
-					'access-control-allow-origin':'*'
-				}
-			}
-		)
-		.then(res => res.json())
-		.then(json => {
-			var state = json[0].ThingState;
-			//not sure why this is caused...
-			if (state) {
-				this.setState(0)
-			} else {
-				this.setState(1)
-			}
-		})
-		.catch(err => {
-			console.log(err);
-		})
-	}
-
-	getID() {
-		return this.id;
-	}
-}
 
 class RGBPin {
 	constructor(pin) {
