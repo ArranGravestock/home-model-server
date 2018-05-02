80e879fd1b9480620d1d36047f4c51b46d7665e8
ArranGravestock <arran@gravestock.net>
2018-05-02 18:37:53 +0100

    -add tests for all components

--- tests.js
+++ tests.js
@@ -1,4 +1,5 @@
 "use strict";
+const fetch = require('node-fetch');
 
 //run setups
 var wpi = require('wiring-pi');
@@ -6,16 +7,139 @@ wpi.setup('wpi');
 
 //import classes
 var Touch = require("./components/touch");
+var LED = require("./components/led");
+var DHT = require("./components/dht");
+var dhtsensor = require("node-dht-sensor");
+var Motion = require("./components/motion");
+var Sound = require("./components/sound");
+var Ultrasonic = require("./components/ultrasonic");
+var Vibration = require("./components/vibration");
+var Fan = require("./components/fan");
 
-exports.testTouch = function(test) {
-	var touch = new Touch(3);
+var config = require("./config.json");
+
+//initiate an empty packet
+var JSON_PACKET = {"DEVICE_ID": config.DEVICE_ID, THINGS: []}
+
+function compilePacket(packet) {
+	var jsonPacket = JSON.stringify(JSON_PACKET)
+
+	//reset packet
+	JSON_PACKET.THINGS = [];
+
+    return jsonPacket;
+}
+
+function sendPacket(jsonpacket) {
+	var PACKET = compilePacket(jsonpacket);
+	console.log(PACKET);
+
+	fetch(`${config.FETCH_IP}/reading`, {headers: { 'Content-Type': 'application/json' }, method: 'PUT', body: PACKET})
+	.then((res) => {
+		if(res.ok) {
+			console.log("SUCCESS")
+		} else {
+			console.log("FAILURE")
+		}
+	})
+	.catch(() => console.log("ERROR"))
+}
+
+/*
+**
+** COMPONENT TESTS
+** RUN AGAINST TEST DB DATA NOT LIVE
+**
+*/
+
+exports.Touch = function(test) {
+	var comp = new Touch(3);
 	
-	touch.readState();
-
-	test.equal(touch.getState(), 0, "assumed no touch state");
-	test.equal(touch.getID(), 3, "id equals pin");
+	test.equal(comp.getState(), 0, "assumed no comp state");
+	test.equal(comp.getID(), 3, "id equals pin");
 
 	test.done();
 }
 
+exports.Led = function(test) {
+	var comp = new LED(7);
+	test.equal(comp.getID(), 7, "id equals pin");
 
+	comp.setState(1);
+	test.equal(comp.getState(), 1, "assumed no comp state");
+
+	comp.fetchState();
+	test.equal(comp.getState(), 0, "state returned from db tests");
+
+	test.done();
+}
+
+exports.DHT = function(test) {
+	var comp = new DHT(26, dhtsensor, 11);
+	test.equal(comp.getID("temp"), 26, "id equals pin");
+	test.equal(comp.getID("humidity"), 27, "id equals pin+1");
+
+	comp.read();
+	test.notEqual(comp.getTemp(), 0, "temp above 0");
+	test.notEqual(comp.getHumidity(), 0, "humidity above 0");
+
+	comp.setHumidity(10);
+	comp.setTemp(15);
+	test.equal(comp.getTemp(), 10, "temp is 10");
+	test.equal(comp.getHumidity(), 15, "humidity is 15");
+
+	test.done();
+}
+
+exports.Fan = function(test) {
+	var comp = new Fan(0);
+
+
+	comp.setState(1);
+	test.equal(comp.getState(), 1, "assumed no comp state");
+
+	comp.fetchState();
+	test.equal(comp.getState(), 0, "state returned from db tests");
+
+	test.done();
+}
+
+exports.Motion = function(test) {
+	var comp = new Motion(4);
+	test.equal(comp.getID(), 4, "id equals pin");
+
+	comp.readState();
+	test.equal(comp.getState(), 0, "assumed no comp state");
+
+	test.done();
+}
+
+exports.Sound = function(test) {
+	var comp = new Sound(4);
+	test.equal(comp.getID(), 4, "id equals pin");
+
+	comp.readState();
+	test.equal(comp.getState(), 0, "assumed no comp state");
+
+	test.done();
+}
+
+exports.Ultrasonic = function(test) {
+	var comp = new Ultrasonic(4, 5);
+	test.equal(comp.getID(), 4, "id equals pin");
+
+	comp.setDistance(10);
+	test.equal(comp.getDistance(), 10, "assumed no comp state");
+
+	test.done();
+}
+
+exports.Vibration = function(test) {
+	var comp = new Vibration(4);
+	test.equal(comp.getID(), 4, "id equals pin");
+
+	comp.readState();
+	test.equal(comp.getState(), 0, "assumed no comp state");
+
+	test.done();
+}
